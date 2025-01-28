7708593d4ad0e468665009b9fd978d8f70afd478
iamaryankr <95643709+iamaryankr@users.noreply.github.com>
2025-01-29 03:23:08 +0530

    appuse

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(cors({ origin: 'https://blog-self-api-backend.vercel.app', credentials: true }));
 
 
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
